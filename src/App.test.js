7efae14f779cbe307bc2f85fd30de915a796ffbf
aakhan <adnan.khan@synavos.com>
2019-04-12 19:27:18 +0500

    :art: added some css changes.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setup = (props = {}) => {
+  return shallow(<App {...props} />);
+};
+
+describe("App Component", () => {
+  let app;
+  beforeEach(() => {
+    app = setup({
+      twitterHref:
+        "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
+    });
+  });
+  it("TwitterHref prop should be present in app Component", () => {
+    const twitterHref = app.props;
+    expect(twitterHref).not.toBeNull();
+  });
+
+  it("Should Render 2 buttons", () => {
+    const buttons = app.find(".button");
+    expect(buttons.length).toBe(2);
+  });
 });
