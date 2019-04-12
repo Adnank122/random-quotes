922fa4f38c2d54378d2f9c0b37e5f02ffd7bd3e3
Adnan Khan <adnan.khan@synavos.com>
2019-04-12 21:25:50 +0500

    Updates

---
+++ precache-manifest.7cb883518e33e1b9f0d951246fcfc167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86fc6191e845fc8e2f60",
+    "url": "/random-quotes/static/css/main.5c69d40d.chunk.css"
+  },
+  {
+    "revision": "86fc6191e845fc8e2f60",
+    "url": "/random-quotes/static/js/main.f8c7dc7f.chunk.js"
+  },
+  {
+    "revision": "9fcc4046b366da42450f",
+    "url": "/random-quotes/static/js/runtime~main.042febda.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/random-quotes/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c0ba123236532e89f589a9b1b853e1c2",
+    "url": "/random-quotes/index.html"
+  }
+];
